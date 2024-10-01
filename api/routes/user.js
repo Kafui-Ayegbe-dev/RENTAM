4c3f3ba05e2e392268d816b330a9bcb59785461a
Kafui-Ayegbe-dev <kafuiayegbedev@gmail.com>
2024-10-01 08:50:31 -0600

    Creating routes for server actions

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,12 +1,5 @@
 const router = require('express').Router();
 
-router.get("/usertest", (req,res)=> {
-    res.send("user test is ok")
-})
 
-router.post("/userposttest", (req,res)=> {
-    const username = req.body.username;
-    res.status(200).send("your uname is: " + username)
-})
 
 module.exports = router
