c6ee60af50b612af247babf2449917d1bd1001cd
Kafui-Ayegbe-dev <kafuiayegbedev@gmail.com>
2025-02-08 22:40:16 -0600

    Added to order logic

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
 const productRoute = require('./routes/product')
 const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
 
 dotenv.config();
 
@@ -19,7 +20,8 @@ app.use(express.json())
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
-app.use('/api/cart', cartRoute)
+app.use('/api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log('Backend is running on port FIVE THOUSAND')
