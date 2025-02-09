d265ae108e933ae5b26e4f09354baa5ddab13fee
Kafui-Ayegbe-dev <kafuiayegbedev@gmail.com>
2025-02-08 21:34:34 -0600

    Added functionality to verify authorisation

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const app = express();
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
+app.use('/api/products', productRoute)
+app.use('/api/cart', cartRoute)
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log('Backend is running on port FIVE THOUSAND')
